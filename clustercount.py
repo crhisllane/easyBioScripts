116f495de12a6dbe227c7c851a8bc5251ab2e048
crhisllane <crhisllane@gmail.com>
2021-02-08 11:37:41 -0300

    take cluster sequence from cdhit

--- clustercount.py
+++ clustercount.py
@@ -74,11 +74,15 @@ for clusterline in allClusterLines:
             cabid = re.search(r"_.*", fastaname1)
             cabid = re.sub("^_", "" ,cabid[0])
             fastaFileName = re.search(r".*\.fna", fastaname1)
-
+            checkname = 0
             if re.match(r"^>PD", fastaname):
-                fastaFileName = fastaFileName[0] + ".genes.fna" 
+                fastaFileName = fastaFileName[0] + ".genes.fna"
+                checkname = re.search(r".ffa_*", fastaname)
+                checkname = re.sub(".ffa_", "" , checkname)
             elif re.match(r"^>SM", fastaname):
-                fastaFileName = fastaFileName[0] + ".ffn" 
+                fastaFileName = fastaFileName[0] + ".ffn"
+                checkname = re.search(r".fna_*", fastaname)
+                checkname = re.sub(".fna_", "" , checkname) 
             else:
                 log.write(fastaname + ' no ^>PD or ^>SM')
 
@@ -88,8 +92,9 @@ for clusterline in allClusterLines:
             fasta_sequences = SeqIO.parse(open(fastaFileName),'fasta')
             for fasta in fasta_sequences:
                 name, sequence = fasta.id, str(fasta.seq)
-                new_sequence = fastaname + '\n' + sequence + '\n'
-                outfasta.write(new_sequence)
-                print ("\t inserting ",  name, "\n") 
+                if checkname in fasta.id:
+                    new_sequence = fastaname + '\n' + sequence + '\n'
+                    outfasta.write(new_sequence)
+                    print ("\t inserting ",  name, "\n") 
 
 
