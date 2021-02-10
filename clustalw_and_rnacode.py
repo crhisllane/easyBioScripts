364f147c5fb4cde6f2a3b06787ce1e749564868a
crhisllane <crhisllane@gmail.com>
2021-02-10 12:27:52 -0300

    clustalw2 e RNAcode

---
+++ clustalw_and_rnacode.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import re
+from optparse import OptionParser
+from Bio import SeqIO
+import concurrent.futures
+
+argv = OptionParser()
+
+argv.add_option("-I", "--Input", action = "store", dest = "file", type ="string",
+                   help = "list of all fasta files do align")
+(argumentos, palha_que_nao_interessa) = argv.parse_args()
+
+clusterfile = open (argumentos.file, 'r')
+allClusterLines = clusterfile.readlines()
+
+#for clusterline in allClusterLines:
+def process_rnacode(clusterline):
+    
+    print (clusterline, "\n")
+
+    cmd1 = "clustalw2 %s"%(clusterline)
+    os.system(cmd1)
+    clusterline_Aln = re.sub(".fasta", ".aln" ,clusterline)
+    clusterline_Pure = re.sub(".fasta", "" ,clusterline)
+    cmd2 = "RNAcode %s -p 0.05 >> %s.tab"%(clusterline_Aln, clusterline_Pure)
+    os.system(cmd2)
+
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=30) as executor:
+    executor.map(process_rnacode, allClusterLines)
