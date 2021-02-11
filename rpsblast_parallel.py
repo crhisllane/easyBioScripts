e0a49c3afd5bda1061c12bb7d9c5d1a767cc337b
crhisllane <crhisllane@gmail.com>
2021-02-11 11:45:53 -0300

    rpsblast parallel

--- rpsblast_parallel.py
+++ rpsblast_parallel.py
@@ -14,7 +14,7 @@ def process_rpsblast(clusterline):
     clusterline = clusterline.rstrip('\n')
     print (clusterline, "\n")
     clusterline_Pure = re.sub(".fasta", "" ,clusterline)
-    cmd1 = "rpsblast+ -db db/Cdd -query %s -out %s_spp.rpsblast -num_threads 40 -evalue 0.01  -outfmt '7 qseqid sseqid pident nident qlen slen length mismatch gapopen qstart qend sstart send evalue bitscore qcovs staxis frames qseq sseq'"%(clusterline, clusterline_Pure)
+    cmd1 = "rpsblast+ -db dbCdd/ -query %s -out %s_CDD.rpsblast -num_threads 40 -evalue 0.01  -outfmt '7 qseqid sseqid pident nident qlen slen length mismatch gapopen qstart qend sstart send evalue bitscore qcovs staxis frames qseq sseq'"%(clusterline, clusterline_Pure)
     os.system(cmd1)
 
 
