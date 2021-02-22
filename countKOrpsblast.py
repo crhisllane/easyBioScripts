58efdae3e1e828e233332ba45590c91209fedf7d
crhisllane <crhisllane@gmail.com>
2021-02-22 15:14:39 -0300

    count KO

---
+++ countKOrpsblast.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import re
+from optparse import OptionParser
+from Bio import SeqIO
+import concurrent.futures
+
+argv = OptionParser()
+
+argv.add_option("-I", "--Input", action = "store", dest = "file", type ="string",
+                   help = "cluster cdhit output file")
+argv.add_option("-l","--list", action="store", dest="list", type="string",
+                    help = "cdhit output fasta file" )
+(argumentos, palha_que_nao_interessa) = argv.parse_args()
+
+KOs_file = open (argumentos.list, 'r')
+KOs_line = KOs_file.readlines()
+
+
+def process_count(rpsfile):
+    
+    rps_open = open (rpsfile, 'r')
+    rps_all_lines = rps_open.readlines()
+    rps_lines = rps_all_lines.rstrip('\n')
+    rps_elements, = rps_lines.split()
+ 
+    if re.match(r"^Query=", rps_elements[0])
+        print ("bateu aqui", rps_elements[0], "\n")
+    
+    if re.match(r"^CDD:", rps_elements[0])
+        print ("-------- aqui", rps_elements[0], "\n")
+        
+
+path = os.getcwd()
+path = str(path) + '/*.rpsblast' 
+files_rpsblast =  glob.glob(path) 
+    
+with concurrent.futures.ProcessPoolExecutor(max_workers=30) as executor:
+    executor.map(process_count, files_rpsblast)
