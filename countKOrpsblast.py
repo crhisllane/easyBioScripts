71f51a30348262e57e51bd91480a16b2660b7138
crhisllane <crhisllane@gmail.com>
2021-02-22 15:21:36 -0300

    count KO

--- countKOrpsblast.py
+++ countKOrpsblast.py
@@ -24,6 +24,7 @@ def process_count(rpsfile):
     rps_all_lines = rps_open.readlines()
     rps_lines = rps_all_lines.rstrip('\n')
     rps_elements, = rps_lines.split()
+    print("teste", rps_elements[0])
  
     if re.match(r"^Query=", rps_elements[0]):
         print ("bateu aqui", rps_elements[0], "\n")
