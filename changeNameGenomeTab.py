38f34e08dd64de6402fbae0d747cecf5a8bb3d58
crhisllane <crhisllane@gmail.com>
2021-02-23 10:11:12 -0300

    change name genome tab

---
+++ changeNameGenomeTab.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import re
+from optparse import OptionParser
+from Bio import SeqIO
+import concurrent.futures
+import glob 
+
+argv = OptionParser()
+
+argv.add_option("-K", "--KOcountfile", action = "store", dest = "file", type ="string",
+                   help = "KO count tab")
+argv.add_option("-f","--first", action="store", dest="firstid", type="string",
+                    help = "first id in eggnog out" )
+
+(argumentos, palha_que_nao_interessa) = argv.parse_args()
+
+eggnog_file = open (argumentos.file, 'r')
+eggnog_lines = eggnog_file.readlines()
+
+
+for eggnog_line in eggnog_lines:
+    if not (eggnog_line.startswith("Genome")):
+        rps_elements = eggnog_line.split("\t")
+        name =  rps_elements[0]
+        print (name, "\n")
