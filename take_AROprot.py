71ff81c030d161a3360a6bbbd47a1f742f9dc1e2
crhisllane <crhisllane@gmail.com>
2021-02-24 11:16:44 -0300

    take fastas of a KO protein

--- take_AROprot.py
+++ take_AROprot.py
@@ -52,6 +52,8 @@ for eggnog_line in eggnog_lines:
             print(name, "\t", kos_annot, "\n")
             comando = "grep %s *.faa "%(name)
             resp = subprocess.check_output(comando, shell=True)
+            resp = re.sub(":>.*", "" ,resp)
+            resp = re.sub("^b\'", "" ,resp)
             resp = str(resp)
             print (resp)
 
