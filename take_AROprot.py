3ee5e1ca00d7bc49d10f32531bb7f86aec30aeb9
crhisllane <crhisllane@gmail.com>
2021-02-24 11:29:30 -0300

    take fastas of a KO protein

--- take_AROprot.py
+++ take_AROprot.py
@@ -39,7 +39,7 @@ Aro_count = [0] * Aro_all
 def take_sequence(sequenceFasta, nameID):
     ini = ">"    
     name_seq, seqs = sequenceFasta.id, str(sequenceFasta.seq)
-    print ("---" name_seq)
+    print ("---", name_seq)
 
 for eggnog_line in eggnog_lines:
     if not (eggnog_line.startswith("#")):
