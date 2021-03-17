30ab1f3accbaa317be481a64327d4580afd17ac9
crhisllane <crhisllane@gmail.com>
2021-03-17 16:32:07 -0300

    rnacode to local

--- clustalw_and_rnacode.py
+++ clustalw_and_rnacode.py
@@ -19,15 +19,15 @@ def process_rnacode(clusterline):
     clusterline = clusterline.rstrip('\n')
     print (clusterline, "\n")
 
-    cmd1 = "clustalw2 %s"%(clusterline)
+    cmd1 = "/home/crhisllane.vasconcelos/clustalw-2.1/src/clustalw2 %s"%(clusterline)
     print ("\t", cmd1, "\n")
     os.system(cmd1)
     clusterline_Aln = re.sub(".fasta", ".aln" ,clusterline)
     clusterline_Pure = re.sub(".fasta", "" ,clusterline)
-    cmd2 = "RNAcode %s -p 0.05 >> %s.tab"%(clusterline_Aln, clusterline_Pure)
+    cmd2 = "/home/crhisllane.vasconcelos/RNAcode-0.3/src/RNAcode %s -p 0.05 >> %s.tab"%(clusterline_Aln, clusterline_Pure)
     print ("\t", cmd2, "\n")
     os.system(cmd2)
 
 
-with concurrent.futures.ProcessPoolExecutor(max_workers=30) as executor:
+with concurrent.futures.ProcessPoolExecutor(max_workers=50) as executor:
     executor.map(process_rnacode, allClusterLines)
