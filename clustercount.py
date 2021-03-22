9672ede4ceaa9c7332f86f090828df3b22cd9066
crhisllane <crhisllane@gmail.com>
2021-03-22 08:25:30 -0300

    take cluster from prodigal and smalprot

--- clustercount.py
+++ clustercount.py
@@ -96,7 +96,8 @@ def process_cdhitcluster(cdhitseq):
                     fasta_sequences = SeqIO.parse(open(fastaFileName),'fasta')
                     for fasta in fasta_sequences:
                         name, sequence = fasta.id, str(fasta.seq)
-                        if re.search(r'.ffn', fastaFileName): 
+                        if re.search(r'.ffn', fastaFileName):
+                            print ("teste do SM0-", name, fastaname, " ", realseqid, " ",  fastaFileName, "\n") 
                             name = re.sub(">[a-zA-Z0-9]*_[a-zA-Z0-9]* ", ">" ,name)
                             print ("teste do SM1-", name, fastaname, " ", realseqid, " ",  fastaFileName, "\n")
                         if realseqid == name:
